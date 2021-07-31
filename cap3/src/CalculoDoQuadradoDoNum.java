a52d46d1718f4c8a5b2dc54a4624aa94294bd0d4
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-07-31 15:47:44 -0300

    create cap3 folder with square calculator

---
+++ cap3/src/CalculoDoQuadradoDoNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class CalculoDoQuadradoDoNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para saber seu valor elevado ao quadrado: ");
+        Integer num = scanner.nextInt();
+
+        int res = num * num;
+
+        System.out.println("O quadrado de " + num + " é " + res);
+
+        scanner.close();
+    }
+}
