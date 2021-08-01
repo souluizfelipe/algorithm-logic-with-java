9c149c37ad5c62f3f969326b687c7ab12198eecc
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-01 15:42:14 -0300

    add primitivesType

---
+++ cap3/src/PrimitiveTypes.java
@@ -0,0 +1,10 @@
+public class PrimitiveTypes {
+    public static void main(String[] args){
+
+        Byte varByte = null;
+        byte varBytePrim = 1; //primitive types start with lowercase letter and can't be attributed a null value (main difference)
+        //String doesn't have a primitive type;
+        //For Integer & Character their primitive counterpart are int & char;
+
+    }
+}
