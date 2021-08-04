3cf18858392a1cbae7d21bd03231670af78cbdfd
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-04 09:41:02 -0300

    add ex02

---
+++ cap6/src/ex02.java
@@ -0,0 +1,15 @@
+public class ex02 {
+    public static void main(String[] args) {
+        Integer[] numbers = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        int i = 0;
+        while(i < numbers.length) {
+            Integer num = numbers[i];
+            if(num % 3 == 0){
+                System.out.print(num + ", ");
+            }
+
+            i++;
+        }
+    }
+}
